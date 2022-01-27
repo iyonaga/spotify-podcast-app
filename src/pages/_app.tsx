f7b7134f6a2a06aa3d1fd4ec82baa13231881112
iyonaga <yiyonaga@gmail.com>
2022-01-28 08:28:47 +0900

    プログレスバー実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import '../styles/globals.css';
 import { NextPage } from 'next';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import nProgress from 'nprogress';
+import { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { RecoilRoot } from 'recoil';
@@ -14,12 +17,36 @@ export type NextApplicationPage<P = {}, IP = P> = NextPage<P, IP> & {
 
 const queryClient = new QueryClient();
 
+nProgress.configure({ showSpinner: false });
+
 function MyApp({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps & {
   Component: NextApplicationPage;
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => {
+      nProgress.start();
+    };
+
+    const handleStop = () => {
+      nProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
